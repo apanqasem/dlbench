778f0ee6ae1ca701d0d62563ca0b39814293b60b
Apan Qasem <apanqasem@gmail.com>
2018-07-20 16:39:34 -0500

    occupancy

--- hc/dlbench.cpp
+++ hc/dlbench.cpp
@@ -161,12 +161,13 @@ void dlbench_device(DATA_ITEM_TYPE *in_a, DATA_ITEM_TYPE *in_b, DATA_ITEM_TYPE *
   t_cp_out = 1.0E6 * (mysecond() - t_cp_out);
 }
 
-void dlbench_device(DATA_ITEM_TYPE *in_a, DATA_ITEM_TYPE *in_b, DATA_ITEM_TYPE s, DATA_ITEM_TYPE *out, int n) {
-
-  const DATA_ITEM_TYPE scalar = s;
+// void dlbench_device_aos(pixel *src, pixel *dst, int n) {
+// }
+void dlbench_device(DATA_ITEM_TYPE *in_a, DATA_ITEM_TYPE *in_b, DATA_ITEM_TYPE *in_c, DATA_ITEM_TYPE *out, int n) {
 
   hc::array<DATA_ITEM_TYPE, 1> d_in_a(n);
   hc::array<DATA_ITEM_TYPE, 1> d_in_b(n);
+  hc::array<DATA_ITEM_TYPE, 1> d_in_c(n);
 
   hc::array<DATA_ITEM_TYPE, 1> d_out(n);
 
@@ -178,14 +179,22 @@ void dlbench_device(DATA_ITEM_TYPE *in_a, DATA_ITEM_TYPE *in_b, DATA_ITEM_TYPE s
   hc::copy(in_b, d_in_b);
   t_cp_in_b = 1.0E6 * (mysecond() - t_cp_in_b);
 
+  t_cp_in_c = mysecond();
+  hc::copy(in_c, d_in_c);
+  t_cp_in_c = 1.0E6 * (mysecond() - t_cp_in_c);
+
   t_par_loop = mysecond();
-  auto future = hc::parallel_for_each(hc::extent<1>(n), [&,scalar](hc::index<1> index) [[hc]] {
-      KERNEL2(d_out[index],d_in_a[index],scalar,d_in_b[index]);
-      DATA_ITEM_TYPE alpha = 2.3;
-      for (int k = 0; k < ITERS; k++)
-	KERNEL1(alpha,alpha,d_in_a[index]);
-      d_out[index] = alpha;
-    });
+  hc::extent<1> ex(THREADS);
+  hc::tiled_extent<1> tiled_ex = ex.tile(WKGRP);
+  auto future = hc::parallel_for_each(tiled_ex, [&](hc::index<1> index) [[hc]] {
+      for (int j = 0; j < SIZE * PIXELS_PER_IMG; j = j + PIXELS_PER_IMG) {
+	DATA_ITEM_TYPE alpha = 2.3;
+	KERNEL2(alpha,d_in_a[index + j],d_in_b[index + j],d_in_c[index + j]);
+	for (int k = 0; k < ITERS; k++)
+	  KERNEL1(alpha,alpha,d_in_a[index + j]);
+	d_out[index + j] = alpha;
+      }
+      });
   future.wait();
   t_par_loop = 1.0E6 * (mysecond() - t_par_loop);
   
@@ -194,13 +203,15 @@ void dlbench_device(DATA_ITEM_TYPE *in_a, DATA_ITEM_TYPE *in_b, DATA_ITEM_TYPE s
   t_cp_out = 1.0E6 * (mysecond() - t_cp_out);
 }
 
-void dlbench_host(DATA_ITEM_TYPE *in_a, DATA_ITEM_TYPE *in_b, DATA_ITEM_TYPE s, DATA_ITEM_TYPE *out, int n) {
+void dlbench_host(DATA_ITEM_TYPE *in_a, DATA_ITEM_TYPE *in_b, DATA_ITEM_TYPE *in_c, DATA_ITEM_TYPE *out, int n) {
 
-  const DATA_ITEM_TYPE scalar = s;
-  hc::array_view<DATA_ITEM_TYPE, 1> av_in_a(n, in_a);
-  hc::array_view<DATA_ITEM_TYPE, 1> av_in_b(n, in_b);
+  //  const DATA_ITEM_TYPE scalar = s;
+  //  hc::array_view<DATA_ITEM_TYPE, 1> av_in_a(n, in_a);
+  //  hc::array_view<DATA_ITEM_TYPE, 1> av_in_b(n, in_b);
+  //  hc::array_view<DATA_ITEM_TYPE, 1> av_in_c(n, in_c);
   //  hc::array_view<DATA_ITEM_TYPE, 1> av_out(n, out);
 
+  //  DATA_ITEM_TYPE *av_in_a = in_a;
   //  hc::array<DATA_ITEM_TYPE, 1> av_in_a(n);
   //  hc::array<DATA_ITEM_TYPE, 1> av_in_b(n);
 
@@ -215,18 +226,23 @@ void dlbench_host(DATA_ITEM_TYPE *in_a, DATA_ITEM_TYPE *in_b, DATA_ITEM_TYPE s, 
   // t_cp_in_b = 1.0E6 * (mysecond() - t_cp_in_b);
 
   t_par_loop = mysecond();
-  //auto future = hc::parallel_for_each(hc::extent<1>(n), [=](hc::index<1> index) [[hc]] {
   //  auto future = hc::parallel_for_each(hc::extent<1>(n), [=,&av_in_a,&av_in_b](hc::index<1> index) [[hc]] {
-  auto future = hc::parallel_for_each(hc::extent<1>(n), [=,&av_out](hc::index<1> index) [[hc]] {
-      KERNEL2(av_out[index],av_in_a[index],scalar,av_in_b[index]);
-      DATA_ITEM_TYPE alpha = 2.3;
-      for (int k = 0; k < ITERS; k++)
-	KERNEL1(alpha,alpha,av_in_a[index]);
-      av_out[index] = alpha;
-      //      av_out[index] = av_in_a[index] +  scalar * av_in_b[index];
-     });
+  hc::extent<1> ex(THREADS);
+  hc::tiled_extent<1> tiled_ex = ex.tile(WKGRP);
+  //  auto future = hc::parallel_for_each(tiled_ex, [=](hc::index<1> index) [[hc]] {
+  auto future = hc::parallel_for_each(tiled_ex, [=,&av_out](hc::index<1> index) [[hc]] {
+      for (int j = 0; j < SIZE * PIXELS_PER_IMG; j = j + PIXELS_PER_IMG) {
+	DATA_ITEM_TYPE alpha = 2.3;
+	KERNEL2(alpha,in_a[index[0] + j],in_b[index[0] + j],in_c[index[0] + j]);
+	for (int k = 0; k < ITERS; k++)
+	  KERNEL1(alpha,alpha,in_a[index[0] + j]);
+	av_out[index + j] = alpha;
+	//      av_out[index] = av_in_a[index] +  scalar * av_in_b[index];
+      }
+    });
   future.wait();
   t_par_loop = 1.0E6 * (mysecond() - t_par_loop);
+
   t_cp_out = mysecond();
   hc::copy(av_out, out);
   t_cp_out = 1.0E6 * (mysecond() - t_cp_out);
@@ -246,8 +262,8 @@ int main() {
   std::cout << "Using HC device " << getDeviceName(current) << std::endl;
 #endif
 
-  unsigned n = SIZE * 1024;
-  unsigned array_size = 1024 * SIZE * sizeof(DATA_ITEM_TYPE);
+  unsigned n = SIZE * PIXELS_PER_IMG;
+  unsigned array_size = n * sizeof(DATA_ITEM_TYPE);
 
   // DATA_ITEM_TYPE *in_c = (DATA_ITEM_TYPE *) malloc(array_size);
   // DATA_ITEM_TYPE *in_d = (DATA_ITEM_TYPE *) malloc(array_size);
@@ -256,31 +272,35 @@ int main() {
   // DATA_ITEM_TYPE *in_g = (DATA_ITEM_TYPE *) malloc(array_size);
   // DATA_ITEM_TYPE *in_h = (DATA_ITEM_TYPE *) malloc(array_size);
 
-  // double t_alloc = mysecond();
-  // DATA_ITEM_TYPE *in_a = (DATA_ITEM_TYPE *) malloc(array_size);
-  // DATA_ITEM_TYPE *in_b = (DATA_ITEM_TYPE *) malloc(array_size);
-  // DATA_ITEM_TYPE *out = (DATA_ITEM_TYPE *) malloc(array_size);
-  // t_alloc = 1.0E6 * (mysecond() - t_alloc);
-
   double t_alloc = mysecond();
-  DATA_ITEM_TYPE *in_a = (DATA_ITEM_TYPE *) am_alloc(array_size, current, amHostPinned);
-  DATA_ITEM_TYPE *in_b = (DATA_ITEM_TYPE *) am_alloc(array_size, current, amHostPinned);
-  DATA_ITEM_TYPE *out = (DATA_ITEM_TYPE *) am_alloc(array_size, current, amHostPinned);
+  DATA_ITEM_TYPE *in_a = (DATA_ITEM_TYPE *) am_alloc(array_size, current, amHostCoherent);
+  DATA_ITEM_TYPE *in_b = (DATA_ITEM_TYPE *) am_alloc(array_size, current, amHostCoherent);
+  DATA_ITEM_TYPE *in_c = (DATA_ITEM_TYPE *) am_alloc(array_size, current, amHostCoherent);
+  //  DATA_ITEM_TYPE *in_a = (DATA_ITEM_TYPE *) malloc(array_size);
+  // DATA_ITEM_TYPE *in_b = (DATA_ITEM_TYPE *) malloc(array_size);
+  // DATA_ITEM_TYPE *in_c = (DATA_ITEM_TYPE *) malloc(array_size);
+  DATA_ITEM_TYPE *out = (DATA_ITEM_TYPE *) malloc(array_size);
   t_alloc = 1.0E6 * (mysecond() - t_alloc);
+
+  // double t_alloc = mysecond();
+  // DATA_ITEM_TYPE *in_a = (DATA_ITEM_TYPE *) am_alloc(array_size, current, amHostPinned);
+  // DATA_ITEM_TYPE *in_b = (DATA_ITEM_TYPE *) am_alloc(array_size, current, amHostPinned);
+  // DATA_ITEM_TYPE *in_c = (DATA_ITEM_TYPE *) am_alloc(array_size, current, amHostPinned);
+  // DATA_ITEM_TYPE *out = (DATA_ITEM_TYPE *) am_alloc(array_size, current, amHostPinned);
+  // t_alloc = 1.0E6 * (mysecond() - t_alloc);
 
   DATA_ITEM_TYPE scalar = 0.5;
   
   double t_init = mysecond();
   for (int i = 0; i < n; i++) {
-    //    in_a[i] = 17;
-    //    in_b[i] = 23;
-    // in_c[i] = 23;
+    in_a[i] = 17;
+    in_b[i] = 23;
+    in_c[i] = 23;
     // in_d[i] = 27;
     // in_e[i] = 29;
     // in_f[i] = 31;
     // in_g[i] = 33;
     // in_h[i] = 35;
-    //    out[i] = 231;
   }
   t_init = 1.0E6 * (mysecond() - t_init);
 
@@ -291,11 +311,11 @@ int main() {
   t_init_out = 1.0E6 * (mysecond() - t_init_out);
   t_kernel = mysecond();
 #ifdef HOST
-  dlbench_host(in_a, in_b, scalar, out, n);
+  dlbench_host(in_a, in_b, in_c, out, n);
   //  dlbench_host(in_a, in_b, in_c, in_d, in_e, in_f, in_g, in_h, scalar, out, n);
 #else 
   //  dlbench_device(in_a, in_b, in_c, in_d, in_e, in_f, in_g, in_h, scalar, out, n);
-  dlbench_device(in_a, in_b, scalar, out, n);
+  dlbench_device(in_a, in_b, in_c, out, n);
 #endif
    t_kernel = 1.0E6 * (mysecond() - t_kernel);
    //   check_results(in_a, in_b, scalar, out, n);
@@ -312,14 +332,16 @@ int main() {
 #endif
 #else
     fprintf(stdout, "%3.8f,%3.8f", t_kernel/1000, t_par_loop/1000);  
-    fprintf(stdout, ",%3.8f,%3.8f", t_alloc/1000,t_init/1000);
+    //    fprintf(stdout, ",%3.8f,%3.8f", t_alloc/1000,t_init/1000);
 #ifdef DEV
-  fprintf(stdout, ",%3.8f,%3.8f,%3.8f", t_cp_in_a/1000,t_cp_in_b/1000,t_cp_out/1000); 
+  fprintf(stdout, ",%3.8f", t_cp_in_a/1000 + t_cp_in_b/1000 + t_cp_in_b/1000); 
+  //  fprintf(stdout, ",%3.8f,%3.8f,%3.8f", t_cp_in_a/1000,t_cp_in_b/1000,t_cp_out/1000); 
    // fprintf(stdout, ",%3.8f", ((array_size)/1.0E9)/((t_init/2 + t_alloc/3 + t_cp_in_a)/1.0E6));
    // fprintf(stdout, ",%3.8f", ((array_size)/1.0E9)/((t_init/2 + t_alloc/3 + t_cp_in_b)/1.0E6));
    // fprintf(stdout, ",%3.8f", (array_size/1.0E9)/((t_init_out + t_alloc/3 + t_cp_out)/1.0E6));
   fprintf(stdout, ",%3.8f", ((array_size)/1.0E9)/((t_cp_in_a)/1.0E6));
   fprintf(stdout, ",%3.8f", ((array_size)/1.0E9)/((t_cp_in_b)/1.0E6));
+  fprintf(stdout, ",%3.8f", ((array_size)/1.0E9)/((t_cp_in_c)/1.0E6));
   fprintf(stdout, ",%3.8f", (array_size/1.0E9)/((t_cp_out)/1.0E6));
 #endif
   fprintf(stdout, "\n");
